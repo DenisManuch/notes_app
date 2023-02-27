21823276c941c02f942177f2f15996ef0f6d32fe
DenisManuch <virtual3161@gmail.com>
2023-02-27 23:10:27 +0200

    corrected for github action 2

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -133,7 +133,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     children: [
                       AutoSizeText(
                         note[index].title,
-                        style: Theme.of(context).textTheme.headline1,
+                        style: Theme.of(context).textTheme.displayLarge,
                         maxLines: maxLinesK,
                         //minFontSize: 20,
                       ),
